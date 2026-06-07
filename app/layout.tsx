5c45a9271b4d6f988fcb98cad3b2acbf756f3059
Aashish-gif <Aashishtejwani12@gmail.com>
2026-06-07 16:22:49 +0530

    Upodated The layout and backend changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/favicon_light.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/favicon_dark.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
